cc3a352e9b995e8ac62a0e67a8b5b46cef40a35d
dinaba1802 <73492440+dinaba1802@users.noreply.github.com>
2024-03-31 21:05:31 +0300

    Home page design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import './HomeFooter.css';
-//import appstore from './images/appstore.png';
-//import playstore from './images/playstore.png';
 
+import React from "react";
+
+import Footer from "./Footer";
 
 export default function Home() {
   return (
     <>
       <div
+        
         style={{ width: "100%", height: "100%", position: "relative" }}
       >
         <img
@@ -46,54 +46,10 @@ export default function Home() {
           >
             Plan Your next Event, easier than ever
           </p>
-        </div>
-        
-      </div>
-      
-
-{/* Footer */}
-<div
-        style={{
-          position: "fixed",
-          bottom: "0",
-          width: "100%",
-          backgroundColor: "#333",
-          color: "#fff",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "20px",
-        }}
-      >
-        {/* Left Section */}
-        <div>
-          <a href="https://www.apple.com/app-store/" target="_blank" rel="noopener noreferrer">
-            <img src="link-to-appstore-image" alt="App Store" style={{ width: "40px", marginRight: "10px" }} />
-          </a>
-          <a href="https://play.google.com/store" target="_blank" rel="noopener noreferrer">
-            <img src="link-to-playstore-image" alt="Play Store" style={{ width: "40px" }} />
-          </a>
-        </div>
-
-        {/* Center Section */}
-        <div style={{ textAlign: "center" }}>
-          <h1>smartEvent</h1>
-          <p>High Quality is our first priority</p>
-          <p>Copyrights 2024 &copy; Dina Baluashvili & Amit Cohen</p>
-        </div>
-
-        {/* Right Section */}
-        <div>
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-            <img src="link-to-instagram-image" alt="Instagram" style={{ width: "40px", marginRight: "10px" }} />
-          </a>
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-            <img src="link-to-facebook-image" alt="Facebook" style={{ width: "40px" }} />
-          </a>
+          <a href="/">Travel Plan</a>
         </div>
       </div>
+      <Footer/>
     </>
-    
-      );
-    }
-  
+  );
+}
